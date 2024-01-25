62847879fc77db8745a0ebdf6ccbe285cd851e9a
Anuj Trehan <anujtrehan@VDL-C02DP8L3P3XY.local>
2024-01-25 13:01:20 +0530

    if else done

---
+++ 12_ifelse/go.mod
@@ -0,0 +1,3 @@
+module my_if_else
+
+go 1.21.1
---
+++ 12_ifelse/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("If Else in go")
+	numUsers :=32
+
+	if numUsers<30{
+		fmt.Println("Users<30")
+	}else {
+		fmt.Println("Users > 30")
+	}
+
+	if num:=3; num<10{
+		fmt.Println("Num<10")
+	}else{
+		fmt.Println("Num>10")
+	}
+}
